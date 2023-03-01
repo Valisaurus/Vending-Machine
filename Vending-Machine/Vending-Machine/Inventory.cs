e38ff5b29e51a56170a32958098bbc448fcf872a
tpku <tpku@icloud.com>
2023-03-02 00:23:40 +0100

    Adjustments to VendingMachine
    Added menu items help, purchase, changed list/choose product > products.
    
    Adjusted ShowProducts()
    - added if statement to not show products out of stock (hardcoded stock values).
    
    Added PurchaseProducts().
    -work in progress.
    
    Added ListHelp().
    
    Added some styling for easier debugging.

--- Vending-Machine/Vending-Machine/Inventory.cs
+++ Vending-Machine/Vending-Machine/Inventory.cs
@@ -2,39 +2,65 @@
 using Vending_Machine;
 namespace Vending_Machine
 {
-	public class Inventory
+    public class Inventory
+    {
 
-	{
-
-
-
-
-        
         public Inventory()
-		{
-            
-	
-		}
+        {
 
+        }
+
+        public Dictionary<string, int> Items { get; set; } = new Dictionary<string, int>
+        {
+            {"Japp", 10},
+            {"Daim", 10},
+            {"Sour-cream & Onion", 10},
+            {"Coca-cola", 10},
+            {"Fanta", 10},
+            {"Sprite", 0},
+            {"Kebab", 0},
+            {"Fesk", 1}
+        };
+
+        public Dictionary<string, int> Prices { get; set; } = new Dictionary<string, int>
+        {
+            {"Japp", 5},
+            {"Daim", 4},
+            {"Sour-cream & Onion", 4},
+            {"Coca-cola", 3},
+            {"Fanta", 3},
+            {"Sprite", 3},
+            {"Kebab", 8},
+            {"Fesk", 10}
+        };
 
 
 
         //public Dictionary<string, int> Items { get; set; } = new Dictionary<string, int>
         //{
+<<<<<<< Updated upstream
         //             {"japp", 10},
         //             {"daim", 10},
         //             {"sour cream onion", 10},
         //             {"coca cola", 10},
         //             {"fanta", 10},
         //             {"sprite", 10},
-
+=======
+        //             "japp",
+        //             "daim",
+        //             "sour cream onion",
+        //             "coca cola",
+        //             "fanta",
+        //             "sprite",
         //};
 
+        //public List<int> Prices { get; set; } = new List<int>
+        //{
+        //    2, 2, 5, 7, 2, 3,
+>>>>>>> Stashed changes
+
+        //};
     }
-
-
-    
-
 }
 
 
--- Vending-Machine/Vending-Machine/VendingMachine.cs
+++ Vending-Machine/Vending-Machine/VendingMachine.cs
@@ -5,9 +5,17 @@ namespace Vending_Machine
     {
         public List<string> Menu { get; } = new List<string>
     {
+<<<<<<< Updated upstream
         "choose products",
         "balance",
           "exit",
+=======
+        "products",
+        "purchase",
+        "balance",
+        "help",
+        "exit",
+>>>>>>> Stashed changes
 
 
     };
@@ -15,21 +23,34 @@ namespace Vending_Machine
 
         public void RunVendingMachine()
         {
-            Console.WriteLine("Welcome to VAT vending-machine! Choose an item");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Welcome to VAT vending-machine!");
             string command;
 
             do
             {
 
                 command = GetMenu();
+<<<<<<< Updated upstream
                 if(command=="choose products")
+=======
+                if (command == "products")
+>>>>>>> Stashed changes
                 {
                     ShowProducts();
                 }
-                else if (command=="balance")
+                else if (command == "purchase")
+                {
+                    PurchaseProduct();
+                }
+                else if (command == "balance")
                 {
                     ShowBalance();
 
+                }
+                else if (command == "help")
+                {
+                    ListHelp();
                 }
 
             }
@@ -42,28 +63,142 @@ namespace Vending_Machine
         {
             while (true)
             {
-                Console.WriteLine("Please choose an option from the menu");
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("Please choose an option from the menu or 'help'");
+                Console.ResetColor();
                 var input = Console.ReadLine();
 
                 if (Menu.Contains(input))
                 {
-                    Console.WriteLine("hej");
+                    Console.WriteLine();
                     return input;
                 }
             }
-
-
         }
 
         public string ShowProducts()
         {
+<<<<<<< Updated upstream
             
+=======
+
+        var ProductStock = new Inventory();
+        var ProductPrices = new Inventory();
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("VAT Products and current stock:");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("-------------------------------");
+            Console.ResetColor();
+            foreach (var price in ProductPrices.Prices)
+            {
+                foreach (var name in ProductStock.Items)
+                {
+                    if (name.Key == price.Key)
+                    {
+                        if (name.Value != 0)
+                        {
+                            Console.Write($"{name.Key} {price.Value}$. Stock: ");
+                            if (name.Value < 3)
+                            {
+                                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                            }
+                            else
+                            {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            }
+                            Console.WriteLine($"{name.Value}");
+                            Console.ResetColor();
+                        }
+                    }
+                }
+            }
+            Console.WriteLine();
+>>>>>>> Stashed changes
         }
         
 
+<<<<<<< Updated upstream
         
+=======
+        public void PurchaseProduct()
+        {
+            //var Stock = new Inventory().Items;
+            var Products = new Inventory().Prices;
+            string choice = null;
+            //selection = Console.ReadLine();
+
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Select a product from the list:");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("-------------------------------");
+            Console.ResetColor();
+
+            while (choice == null)
+            {
+
+                foreach (var product in Products)
+                {
+                    Console.Write($"{product.Key} ");
+                }
+                Console.Write(": ");
+                choice = Console.ReadLine();
+                Console.WriteLine();
+                //Console.WriteLine(choice);
+
+                foreach (var product in Products)
+                {
+                    if (choice == product.Key)
+                    {
+                        Console.Write($"You selected {product.Key}. Do you want to purchase it? {product.Value}$ (yes / no): ");
+                        choice = Console.ReadLine();
+                        Console.WriteLine();
+
+                        if (choice == "yes")
+                        {
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            Console.WriteLine("Thank you for choosing VAT!");
+                            Console.ResetColor();
+                        }
+
+                        if (choice == "no")
+                        {
+                            choice = null;
+                        }
+                    }
+
+                }
+
+                Console.WriteLine();
+
+            }
+
+            
+
+
+        }
+
+        public void ListHelp()
+        {
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("         Menu Selection");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("-------------------------------");
+            Console.ResetColor();
+
+            foreach (var menuItem in Menu)
+            {
+                if (menuItem != "help")
+                {
+                    Console.WriteLine(menuItem);
+                }
+            }
+
+            Console.WriteLine();
+        }
+>>>>>>> Stashed changes
     }
-
-
 }
 
